1f06f5c7a94a118ba3b46cff6ba507699408c22c
zarawiicu <zarasaa05@gmail.com>
2024-12-01 14:51:23 +0700

    Fixing AuthController dengan JWT Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+//use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -15,7 +17,8 @@ class AuthController extends Controller
         $request->validate([
             'username' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
+            'password' => 'required|string|min:5|confirmed',
+            'role' => 'required'
         ]);
 
         $user = User::create([
@@ -37,20 +40,48 @@ class AuthController extends Controller
             'password' => 'required|string'
         ]);
 
-        $user = User::where('email', $request->email)->first();
+        $credentials = $request->only('email', 'password');
 
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+        if(!$token = auth()->guard('api')->attempt($credentials)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Email atau Password Anda Salah',
+            ], 401);
         }
 
-        $token = $user->createToken('auth_token')->plainTextToken;
-
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'user' => $user
-        ]);
+            'success' => true,
+            'user' => auth()->guard('api')->user(),
+            'token' => $token
+        ], 200);
+    }
+
+    public function logout()
+    {
+        try {
+            // Mendapatkan token dari request
+            $token = JWTAuth::getToken();
+
+            // Menghapus atau meng-invalidasi token
+            if ($token) {
+                JWTAuth::invalidate($token);
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Logout berhasil!',
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Token tidak ditemukan.',
+                ], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Logout gagal, token tidak valid atau sudah kedaluwarsa.',
+                'error' => $e->getMessage(),
+            ], 500);
+        }
     }
 }
